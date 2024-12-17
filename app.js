290cb660d8a90ddf94953cb5057903d0fe0b489d
redyouseff <yousefalkret2@gmail.com>
2024-12-17 23:55:49 +0200

    test

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ const corsOptions = {
       "http://192.168.1.6:3000",  // Allow local development with specific IP
       "https://joyav3.vercel.app",  // Allow production frontend URL
       "https://joya-proprties-front.vercel.app", 
+      "https://joyaproperties.com",  
     ], 
     methods: ["GET", "POST", "PUT", "DELETE"],  // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"],  // Allowed headers
